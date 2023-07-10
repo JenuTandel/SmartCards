21a3824a1b4bf98e8f6cf82a4228025f23993430
Jinal Tandel <jinal.tandel@1rivet.local>
2023-07-10 18:50:36 +0530

    open overlay

--- src/app/core/services/overlay.service.ts
+++ src/app/core/services/overlay.service.ts
@@ -1,32 +1,51 @@
-import { ComponentRef, Injectable } from '@angular/core';
+import { ComponentRef, ElementRef, Injectable } from '@angular/core';
 import { Overlay } from '@angular/cdk/overlay';
 import { ComponentPortal } from '@angular/cdk/portal';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OverlayService {
   public instance: any;
+  public closeDialog: Subject<boolean>;
 
-  constructor(private overlay: Overlay) {}
+  constructor(private overlay: Overlay) {
+    this.closeDialog = new Subject();
+  }
 
-  openDialog(component: any): ComponentRef<any> {
-    const positionStrategy = this.overlay.position().global();
-    // .centerHorizontally()
-    // .centerVertically();
+  openDialog(component: any, el: ElementRef, name: string): ComponentRef<any> {
+    // const positionStrategy = this.overlay.position().global();
 
     // Create the overlay with customizable options
     const overlayRef = this.overlay.create({
-      positionStrategy,
       hasBackdrop: true,
+      backdropClass: 'cdk-overlay-transparent-backdrop',
+      positionStrategy: this.overlay
+        .position()
+        .flexibleConnectedTo(el)
+        .withPositions([
+          {
+            originX: 'end',
+            originY: 'bottom',
+            overlayX: 'end',
+            overlayY: 'top',
+            offsetY: 10,
+          },
+        ]),
     });
-
     const portal = new ComponentPortal(component);
     this.instance = overlayRef.attach(portal);
+    this.instance.name = name;
 
     // Close the dialog using backdropClick()
     overlayRef.backdropClick().subscribe(() => {
       overlayRef.detach();
+    });
+    this.closeDialog.subscribe((res) => {
+      if (res == true) {
+        overlayRef.detach();
+      }
     });
 
     return this.instance;
